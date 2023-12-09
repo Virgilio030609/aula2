891a16e9f742a1770069e214f7b90170cdc6eb92
@virgilio030609 <eng.virgiliobernardo@hotmail.com>
2023-12-09 12:35:19 +0000

    inicio do projeto

---
+++ src/main/java/curso/jakarta/aula2/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package curso.jakarta.aula2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
